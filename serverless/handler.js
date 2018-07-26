a2fd71f7acf82b78c898daa130282470e477446a
Sean Wilkinson <swilkinson09@gmail.com>
2018-07-26 11:11:28 -0400

    Look for IP address combination in dynamodb table, if found return item. If not create new item and assign new uuid.

--- serverless/handler.js
+++ serverless/handler.js
@@ -2,17 +2,76 @@
 const uuid = require('uuid')
 const dynamoDb = require('./dynamodb')
 
+
+const createUser = (internalIp, externalIp, callback) => {
+  const putParams = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      userId: uuid.v1(),
+      extIp: externalIp,
+      intIp: internalIp
+    }
+  }
+  dynamoDb.put(putParams, (error) => {
+    if (error) {
+      // console.log('Put Error: ' + error)
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: {'Content-Type': 'application/json'},
+        body: {
+          'message': 'Cannot get user',
+          'error': error
+        }
+      })
+      return
+    }
+    // console.log('New User: {' + internalIp + ', ' + externalIp + ', ' + putParams.Item.userId + '}')
+    const response = {
+      statusCode: 200,
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(putParams.Item)
+    }
+    callback(null, response)
+  })
+}
+
 module.exports.getUserId = (event, context, callback) => {
+  const externalIp = event.requestContext.identity.sourceIp || '0.0.0.0'
+  const internalIp = event.headers.internalIp || '0.0.0.0'
+  // console.log('External IP ' + externalIp)
+  // console.log('Internal IP ' + internalIp)
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      'extIp': externalIp,
+      'intIp': internalIp
+    }
+  }
 
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      // console.error(error)
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'application/json' },
+        body: {
+          'message': 'Cannot get user',
+          'error': error
+        }
+      })
+      return
+    }
 
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Get User',
-      input: event,
-    }),
-  };
-  callback(null, response);
-
+    if (result.Item) {
+      const response = {
+        statusCode: 200,
+        headers: {'Content-Type': 'application/json'},
+        body:  JSON.stringify(result.Item)
+      }
+      callback(null, response)
+    } else {
+      // console.log('User unrecognized. Create user with: {' + internalIp + ', ' + externalIp + '}')
+      createUser(internalIp, externalIp, callback)
+    }
+  })
 }
